d0307d76d82df95e670814e8c4b6e7efdfbfca98
James Childers <jchilders62@gmail.com>
2014-05-09 11:58:58 -0400

    wrapping up plugin sans tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,20 +33,9 @@ module.exports = function (grunt) {
 
     // Configuration to be run (and then tested).
     hologram: {
-      default_options: {
+      generate: {
         options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+          config: 'test/hologram_config.yml'
         }
       }
     },
